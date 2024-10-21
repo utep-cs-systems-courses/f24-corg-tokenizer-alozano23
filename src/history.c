d40ed1d633892749b1567eae1c8cda4815c90750
student <alozano23@miners.utep.edu>
2024-10-21 15:52:40 -0600

    idk

--- src/history.c
+++ src/history.c
@@ -13,32 +13,64 @@
 /* Initialize the linked list to keep the history. */
 
 List* init_history(){
+  struct s_List* history = (struct s_List*)malloc(sizeof(struct s_List*));
 
-  struct s_Item* root= (struct s_Item*)calloc(1, sizeof(struct s_Item) );
-
-  struct s_Item* next_node = (struct s_Item*)calloc(1, sizeof(struct s_Item) );
-
-  root->id = 0;
-
-  root->next = next_node;
-
-  struct s_List* list = (struct s_List*)malloc(sizeof(struct s_List) );
-
-  list->root = root;
-
-  return list;
-
+  if(history == NULL){
+    printf("history not initialized by init_history() ");
+    return NULL;
+  }
+  history->root = NULL;
+  return history;
 }
 
 void add_history(List* list, char* string){
+  if(list == NULL){
+    printf("No history created");
+    return;
+  }
+  if(string == NULL){
+    printf("No viable string found!");
+    return;
+  }
+  struct s_Item* histEntry = (struct s_Item*)malloc(sizeof(struct s_Item*));
+
+  if(list->root == NULL){
+    list->root = histEntry;
+    list->root->str = string;
+    list->root->next = NULL;
+    return;
+  }
+  struct s_Item* temp = list->root;
+  while(temp->next != NULL){
+    temp = temp->next;
+  }
+  temp->next = histEntry;
+  temp->next->str = string;
   return;
 }
 
 void print_history(List* list){
+  if(list == NULL){
+    printf("History not found");
+    return;
+  }
+  struct s_Item* temp = list->root;
+  while(temp != NULL){
+    printf("%d. %s\n", temp->id, temp->str);
+    temp = temp->next;
+  }
   return;
 }
 
 void free_history(List* list){
+  if (list == NULL)return;
+  struct s_Item* temp = list->root;
+
+  while(temp != NULL){
+    free(temp);
+    temp = temp->next;
+  }
+  free(list);
   return;
 }
 
--- src/uimain.c
+++ src/uimain.c
@@ -4,14 +4,42 @@
 #include "history.h"
 
 int main(){
+  struct s_List* list = init_history();
+ start:
   printf("type something below\n");
   printf("> ");
   char in_len [256];
   fgets(in_len, sizeof(in_len), stdin); 
-    
+  add_history(list, in_len);  
   char** token = tokenize(in_len);
-  
+    
   print_tokens(token);
   free_tokens(token);
+  goto print;
+  
+ print:
+  printf("Do you want to print the history?\n");
+  fgets(in_len, sizeof(in_len), stdin);
+  if( in_len[0] =='y' | in_len[0] =='Y'){
+    print_history(list);
+    goto quit;
+  }
+  else if(in_len[0] =='N'| in_len[0] == 'n'){
+    goto quit;
+  }else{
+    printf("misinput. Try Again!\n");
+    goto print;
+  }
+ quit:
+  printf("would you like to quit?\n");
+  fgets(in_len, sizeof(in_len), stdin);
+  if(in_len[0] == 'y' | in_len[0] =='Y' ){
+    free_history(list);
+    goto end;
+  }
+  goto start;
+
+ end:
+  printf("Goodbye\n");
   
 }
